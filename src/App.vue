01046006fb1f5c4112d698aa98d7ab138bfaa2ff
hannydevelop <ukpaiugochiibem@gmail.com>
2020-12-07 12:24:53 +0100

    fixing slot components

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,119 @@
 
 <template>
   <div id="app">
-    <h1>Simple App that explains Vue Slots</h1>
-    <Buttons>
-    <button slot="green" class="Green">I am Green</button>
-    </Buttons>
-     <Buttons>
-    <button slot="blue" class="Blue">I am Blue</button>
-    </Buttons>
-     <Buttons>
-    <button slot="red" class="Red">I am Red</button>
-    </Buttons>
-    <Buttons>
-    <button slot="red" class="Blue">I am Red</button>
-    <button slot="red" class="Red">I am Red</button>
-    <button slot="red" class="Green">I am Red</button>
-    </Buttons>
+   <form-helper>
+     <h1 slot="title1">Welcome to Form One!</h1>
+     <form slot="form1">
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Email address</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlSelect1">Example select</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlSelect2">Example multiple select</label>
+    <select multiple class="form-control" id="exampleFormControlSelect2">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Example textarea</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+</form>
+<h1 slot="title2">Welcome to Form Two!</h1>
+<form slot="form2">
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Email address</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlSelect1">Example select</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlSelect2">Example multiple select</label>
+    <select multiple class="form-control" id="exampleFormControlSelect2">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Example textarea</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+</form>
+<h1 slot="title3">Welcome to Form Three!</h1>
+<form slot="form3">
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Email address</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlSelect1">Example select</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlSelect2">Example multiple select</label>
+    <select multiple class="form-control" id="exampleFormControlSelect2">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Example textarea</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+</form>
+    <button slot="button1" type="button" class="btn btn-primary">Primary</button>
+    <button slot="button2" type="button" class="btn btn-secondary">Secondary</button>
+    <button slot="button3" type="button" class="btn btn-success">Success</button>
+    <button slot="button4" type="button" class="btn btn-danger">Danger</button>
+    <button slot="button5" type="button" class="btn btn-warning">Warning</button>
+    <button slot="button6" type="button" class="btn btn-info">Info</button>
+    <button slot="button7" type="button" class="btn btn-light">Light</button>
+    <button slot="button8" type="button" class="btn btn-dark">Dark</button>
+  </form-helper>
   </div>
 </template>
 <script>
-import Buttons from './components/Buttons'
+import formHelper from './components/Forms.vue'
+import buttonHelper from './components/Buttons.vue'
 export default {
   name: 'App',
   components: {
-    Buttons,
+    'form-helper':formHelper,
+    'button-helper':buttonHelper
   }
     }
 </script>
--- src/components/Forms.vue
+++ src/components/Forms.vue
@@ -1,7 +1,26 @@
 <template>
+<div>
   <div>
-    <slot name="salesPerson"></slot>
-    <slot name="manager"></slot>
-    <slot name="accountDept"></slot>
+    <slot name="title1"></slot>
+    <h2>I am the form helper</h2>
+    <slot name="form1"></slot>
+    <slot name="button1"></slot>
   </div>
+  <div>
+    <slot name="title2"></slot>
+    <slot name="form2"></slot>
+    <slot name="button2"></slot>
+  </div>
+  <div>
+    <slot name="title3"></slot>
+    <slot name="form3"></slot>
+    <slot name="button3"></slot>
+  </div>
+</div>
 </template>
+
+<script>
+export default {
+  name: 'Forms',
+    }
+</script>
